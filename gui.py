48daf3c4c0812dde97f54b53138e2d3c37b77116
raleighcarsone <91168850+raleighcarsone@users.noreply.github.com>
2023-02-13 17:45:22 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu Nov 17 11:23:05 2022
 
 @author: 15129
 """
 
-
+import tkinter
 from tkinter import *
 from tkinter import ttk
 from tkinter.ttk import  Label
@@ -22,7 +21,6 @@ import globe
 import satList
 import move
 import tracking
-import pandas as pd
 from pandas import DataFrame
 
 
@@ -50,19 +48,19 @@ tabs.add(satListFrame, text="Sat List")
 #Manual Control Tab
 
 button1=ttk.Button(userControlFrame, text="Up", command=move.up)
-button1.grid(row=10,column=20)
+button1.place(y=50,x=100)
 
 button2=ttk.Button(userControlFrame, text="Down", command=move.down)
-button2.grid(row=30,column=20)
+button2.place(y=100,x=100)
 
 button3=ttk.Button(userControlFrame, text="Left",command=move.left)
-button3.grid(row=20,column=10)
+button3.place(y=100,x=0)
 
 button4=ttk.Button(userControlFrame, text="Right", command= move.right)
-button4.grid(row=20,column=30)
+button4.place(y=100,x=200)
 
 button5=ttk.Button(userControlFrame, text="Auto Tracking", command=tracking.autoTrack)
-button5.grid(row=50,column=50)
+button5.place(y=0,x=0)
 
 
 
@@ -72,8 +70,51 @@ isometricview = Button(visualFrame, command = globe.globe(visualFrame), text = "
 
 
 #Sat List
-button6=ttk.Button(satListFrame ,text="Open Sat Table", command = satList.openFile(satListFrame))
-button6.grid(row=50,column=50)
+tree = ttk.Treeview(satListFrame)
+tree.place(y=50)
+tree.pack(expand=tkinter.TRUE)
+def openFile():
+
+   filename = filedialog.askopenfilename(title="Open a File", filetype=(("xlxs files", ".*xlsx"),
+("All Files", "*.")))
+
+   if filename:
+      try:
+         filename = r"{}".format(filename)
+         df = pd.read_excel(filename)
+      except ValueError:
+         label.config(text="File could not be opened")
+      except FileNotFoundError:
+         label.config(text="File Not Found")
+
+   # Clear all the previous data in tree
+   clear_treeview()
+
+   # Add new data in Treeview widget
+   tree["column"] = list(df.columns)
+   tree["show"] = "headings"
+
+   # For Headings iterate over the columns
+   for col in tree["column"]:
+      tree.heading(col, text=col)
+
+   # Put Data in Rows
+   df_rows = df.to_numpy().tolist()
+   for row in df_rows:
+         tree.insert("", "end", values=row)
+
+   tree.pack()
+
+
+# Clear the Treeview Widget
+def clear_treeview():
+   tree.delete(*tree.get_children())
+
+
+
+button6=ttk.Button(satListFrame ,text="Open Sat Table", command = openFile)
+button6.place(y=0,x=0)
+
 
 
 
