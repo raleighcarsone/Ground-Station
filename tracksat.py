b511dafc4e3a3c60f5d98f87685129316f7f00b4
raleighcarsone <91168850+raleighcarsone@users.noreply.github.com>
2023-03-30 15:23:39 -0400

    Update tracksat.py

--- tracksat.py
+++ tracksat.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Feb 13 14:36:07 2023
+This script loads a TLE file from Celestrak, which provides orbital data for many satellites. You will need to replace the satellite_name variable with the name of the satellite you want to track, and you may need to adjust the URL of the TLE file if you want to track a satellite that is not included in the default file.
+
+The script then defines the location of the ground station using latitude, longitude, and elevation, and gets the current time using the skyfield library's timescale object.
+
+Finally, the script calculates the satellite's position relative to the ground station using the difference object, which calculates the vector between the satellite and the ground station at the given time. The topocentric object then converts this vector to a topocentric coordinate system (i.e. relative to the ground station). The script prints the altitude and azimuth of the satellite at the given time.
+
+To track the satellite over time, you can put this code in a loop that updates the time and recalculates the satellite's position at regular intervals. You can also use this script as a starting point to build a more sophisticated tracking system that controls a motorized antenna to keep the satellite in view.
 
 @author: 15129
 """
@@ -8,6 +15,7 @@ Created on Mon Feb 13 14:36:07 2023
 
 from skyfield.api import load, Topos
 import time 
+import serialCommunication as sercom
 
 altitude = 0
 azimuth = 0
@@ -27,10 +35,10 @@ def tracking(satname):
             raise ValueError('Satellite not found')
         
         # Define location of ground station
-        latitude = 40.0  # in degrees
-        longitude = -105.0  # in degrees
-        elevation = 2000  # in meters
-        ground_station = Topos(latitude, longitude, elevation)
+        latitude = 29.18368  # in degrees
+        longitude = -81.043233  # in degrees
+        altitude1 = -12  # in meters
+        ground_station = Topos(latitude, longitude, altitude1)
         
         # Get current time
         timeScale = load.timescale()
@@ -39,11 +47,14 @@ def tracking(satname):
         # Calculate satellite's position relative to ground station
         difference = satellite - ground_station
         topocentric = difference.at(currentTime)
-        altitude, azimuth, distance = topocentric.altaz()
+        elevation, azimuth = topocentric.altaz()
         
-        # Print results
-        print('Satellite altitude:', altitude)
+        # Print results and send to the ground station 
+        print('Satellite elevation:', elevation)
         print('Satellite azimuth:', azimuth)
-        
+        currentLocation=sercom.recieve(elevation,azimuth)
+        nextLocation=sercom.sendAngle(elevation , azimuth)
+        while (currentLocation != nextLocation):
+           currentLocation=sercom.recieve(elevation,azimuth)
         time.sleep(1)
-  
+tracking('ISS (ZARYA)');
